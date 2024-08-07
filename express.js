63ccf0a1beab6044c45d988cb7df6ddbb3bec1b3
amanda <amandadecassiaborges2@gmail.com>
2024-08-06 22:26:32 -0300

    Inicializacao de modulos e servidores

--- express.js
+++ express.js
@@ -6,6 +6,12 @@ const app = express() //Inicializando express
 app.get("/", function(requisicao, resposta){
     resposta.send("Bem vindo a rota Home com Express!")
 })
+app.get("/cliente", function(requisicao, resposta){
+    resposta.send("Listando cliente...!")
+})
+app.get("/cliente/cadastro", function(requisicao, resposta){
+    resposta.send("Cadastrando clientes!")
+})
 
 // Iniciando servidor
 
