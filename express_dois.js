d3991236a9448c3c83456ce3d54f920da90dadbc
amanda <amandadecassiaborges2@gmail.com>
2024-08-13 22:32:29 -0300

    criando rotas

--- express_dois.js
+++ express_dois.js
@@ -2,25 +2,68 @@ const express = require("express") // importando express
 
 const app = express() // inicializando express
 
+const path = require('path') // importando core module
+const basePath = path.join(__dirname,'templates')
+
+app.use(
+    express.urlencoded({
+        extended:true
+    })
+)
+
+app.use(express.json())
+
+
+app.get('/users/add', (req, res)=>{
+    res.sendFile(basePath + '/form.html')
+})
+
+app.post('/users/save', (req, res)=>{
+    console.log(req.body)
+    console.log(req.body.name)
+    console.log(req.body.idade)
+
+    res.sendFile(basePath + '/form.html')
+})
+
+
 // criando rotas
-app.get("/", function(requisicao, resposta) {
-    resposta.send("Bem vindo a rota Home com Express!")
-})
+// app.get("/", function(requisicao, resposta) {
+//     resposta.send("Bem vindo a rota Home com Express!")
+// })
+// const checkAuth = function(req,res,next){
+//     req.authStatus = false
 
-app.get("/cliente", function(requisicao, resposta) {
-    resposta.send("Listando Cliente...")
-})
+//     if(req.authStatus){
+//         console.log('Está logado, pode continuar')
+//         next()
+//     }else {
+//         console.log('nao esta logado.')
+//         next()
+//     }
+// }
 
-app.get("/cliente/cadastro", function(requisicao, resposta) {
-    resposta.send("Cadastrando Clientes na pagina!")
-})
+// app.use(checkAuth)
 
-// passagem de paramentros
+// app.get("/", function(requisicao, resposta) {
+//     console.log("enviando arquivo")
+//     resposta.sendFile(basePath + '/index.html')
+// })
 
-app.get("/ola/:nome", function(requisicao, resposta) {
-    var nome = "<h2>Oi " + requisicao.params.nome + ". Seja Bem Vindo(a)!</h2"
-    resposta.send(nome)
-})
+// app.get("/cliente", function(requisicao, resposta) {
+//     resposta.send("Listando Cliente...")
+// })
+
+// app.get("/cliente/cadastro", function(requisicao, resposta) {
+//     resposta.send("Cadastrando Clientes na pagina!")
+// })
+
+// // passagem de paramentros
+
+// app.get("/ola/:nome", function(requisicao, resposta) {
+//     var nome = "<h2>Oi " + requisicao.params.nome + ". Seja Bem Vindo(a)!</h2"
+//     resposta.send(nome)
+// })
 
 // app.get("/user/:cpf?", function(requisicao, resposta) {
 //     var cpf = requisicao.params.cpf
@@ -43,26 +86,26 @@ app.get("/ola/:nome", function(requisicao, resposta) {
 //     }
 // })
 
-app.get("/calculadora", function(req, res) {
-    var valor = parseInt(req.query["valor"]);
-    var valor2 = parseInt(req.query["valor2"]);
-    var operacao = req.query["operacao"];
-    var resultado;
+// app.get("/calculadora", function(req, res) {
+//     var valor = parseInt(req.query["valor"]);
+//     var valor2 = parseInt(req.query["valor2"]);
+//     var operacao = req.query["operacao"];
+//     var resultado;
 
-    switch(operacao) {
-        case "soma":
-            resultado = valor + valor2;
-            res.send("Resultado: " + resultado);
-            break;
-        case "subtrai":
-            resultado = valor - valor2;
-            res.send("Resultado: " + resultado);
-            break;
-        default:
-            res.send("Operação inválida");
-            break;
-    }
-})
+//     switch(operacao) {
+//         case "soma":
+//             resultado = valor + valor2;
+//             res.send("Resultado: " + resultado);
+//             break;
+//         case "subtrai":
+//             resultado = valor - valor2;
+//             res.send("Resultado: " + resultado);
+//             break;
+//         default:
+//             res.send("Operação inválida");
+//             break;
+//     }
+// })
 
 
 // iniciando servidor
